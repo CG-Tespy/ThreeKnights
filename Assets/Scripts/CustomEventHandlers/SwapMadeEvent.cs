55fcb33ce1dfedc6b792121e8badb34362331104
CG-Tespy <gabriel.cabrera257@hotmail.com>
2019-08-10 05:47:13 -0400

    Fixed slight issues

---
+++ Assets/Scripts/CustomEventHandlers/SwapMadeEvent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Fungus;
+
+[EventHandlerInfo("Three Knights",
+                      "Match Made",
+                      "The block will execute when a tile swap is made.")]
+    [AddComponentMenu("")]
+public class SwapMadeEvent : EventHandler
+{
+    public static TileSwapArgs lastSwapMade             { get; protected set; }
+    public static void Invoke(TileSwapArgs swapMade)
+    {
+        if (swapMade != null)
+            lastSwapMade =                              swapMade;
+        foreach (SwapMadeEvent responder in FindObjectsOfType<SwapMadeEvent>())
+        {
+            responder.ExecuteBlock();
+        }
+    }
+}
---
+++ Assets/Scripts/TileSpecific/TileSwapArgs.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Contains details about a tile-swapping that happened.
+/// </summary>
+public class TileSwapArgs : System.EventArgs
+{
+    protected List<TileController> tilesInvolved =          new List<TileController>();
+
+    public List<TileController> TilesInvolved
+    {
+        get { return tilesInvolved; }
+    }
+}
