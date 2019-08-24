46f55b8ed639da357b2a7106c79b5040685e671b
CG-Tespy <gabriel.cabrera257@hotmail.com>
2019-08-24 13:41:27 -0400

    Added some events and commands, setting up for more mechanics to be implemented

---
+++ Assets/Scripts/CustomCommands/InvokeGameOverEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using Fungus;
+
+[CommandInfo("Three Knights", 
+                 "Invoke Game Over Event", 
+                 "Runs blocks set to react to the Game Over event")]
+    [AddComponentMenu("")]
+public class InvokeGameOverEvent : Command
+{
+   
+    public override void OnEnter()
+    {
+        base.OnEnter();
+        GameOverEvent.Invoke();
+    }
+}
---
+++ Assets/Scripts/CustomEventHandlers/GameOverEvent.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using Fungus;
+
+[EventHandlerInfo("Three Knights",
+                      "Game Over",
+                      "The block will execute when it's Game Over.")]
+    [AddComponentMenu("")]
+public class GameOverEvent : EventHandler
+{
+    public static void Invoke()
+    {
+        foreach (GameOverEvent gameOverEvent in FindObjectsOfType<GameOverEvent>())
+        {
+            gameOverEvent.ExecuteBlock();
+        }
+    }
+}
